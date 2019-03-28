698bf31dd948beced89034a6ff32aa476b5dd090
Huijun Wang <huijunwang@usc-guestwireless-upc-newsc6671.usc.edu>
2019-03-28 13:21:40 -0700

    Updates

---
+++ precache-manifest.c17303a866956e9ca6fd3558866c64f9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/home_app/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "763e535505e561e44299",
+    "url": "/home_app/static/js/runtime~main.80ebfe30.js"
+  },
+  {
+    "revision": "ee246ba5523a3894c98a",
+    "url": "/home_app/static/js/main.2f07ed66.chunk.js"
+  },
+  {
+    "revision": "0571b69cddc311e66ec0",
+    "url": "/home_app/static/js/2.b934461c.chunk.js"
+  },
+  {
+    "revision": "ee246ba5523a3894c98a",
+    "url": "/home_app/static/css/main.09c35361.chunk.css"
+  },
+  {
+    "revision": "98f5284c45b5dd7f7a9cdc50d1b53e0f",
+    "url": "/home_app/index.html"
+  }
+];

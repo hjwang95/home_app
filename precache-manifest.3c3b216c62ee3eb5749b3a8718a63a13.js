ef47a981bca81ad65c55a60cf4f4457b7f17f8bd
Huijun Wang <huijunwang@Huijuns-MacBook-Pro.local>
2019-03-31 03:17:02 -0700

    Updates

---
+++ precache-manifest.3c3b216c62ee3eb5749b3a8718a63a13.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "763e535505e561e44299",
+    "url": "/home_app/static/js/runtime~main.80ebfe30.js"
+  },
+  {
+    "revision": "981c81d171fa7f363fb8",
+    "url": "/home_app/static/js/main.c3c5e5ad.chunk.js"
+  },
+  {
+    "revision": "5904f96093d2ffc179e6",
+    "url": "/home_app/static/js/2.81af5636.chunk.js"
+  },
+  {
+    "revision": "981c81d171fa7f363fb8",
+    "url": "/home_app/static/css/main.cc57632b.chunk.css"
+  },
+  {
+    "revision": "5904f96093d2ffc179e6",
+    "url": "/home_app/static/css/2.94e4c649.chunk.css"
+  },
+  {
+    "revision": "15e5853c63f0bcdf57334f1bf1c874fc",
+    "url": "/home_app/index.html"
+  }
+];

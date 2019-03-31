befe91468373ffb65af9993649f7ff409492eb72
Huijun Wang <huijunwang@Huijuns-MacBook-Pro.local>
2019-03-31 03:44:30 -0700

    fixhttps://hjwang95.github.io/ problem

--- build/precache-manifest.ed3af842d3e7d659493434b8961793ce.js
+++ build/precache-manifest.ed3af842d3e7d659493434b8961793ce.js
@@ -4,15 +4,15 @@ self.__precacheManifest = [
     "url": "/home_app/static/js/runtime~main.80ebfe30.js"
   },
   {
-    "revision": "981c81d171fa7f363fb8",
-    "url": "/home_app/static/js/main.c3c5e5ad.chunk.js"
+    "revision": "55b3e62e8412c109070d",
+    "url": "/home_app/static/js/main.728ca8c9.chunk.js"
   },
   {
     "revision": "5904f96093d2ffc179e6",
     "url": "/home_app/static/js/2.81af5636.chunk.js"
   },
   {
-    "revision": "981c81d171fa7f363fb8",
+    "revision": "55b3e62e8412c109070d",
     "url": "/home_app/static/css/main.cc57632b.chunk.css"
   },
   {
@@ -20,7 +20,7 @@ self.__precacheManifest = [
     "url": "/home_app/static/css/2.94e4c649.chunk.css"
   },
   {
-    "revision": "15e5853c63f0bcdf57334f1bf1c874fc",
+    "revision": "d16381dd59366b4a871091d86eb4b88e",
     "url": "/home_app/index.html"
   }
 ];
